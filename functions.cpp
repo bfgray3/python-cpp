236733f5b2b4df113f723a9f633add3bb057540e
Bernie Gray <bfgray3@gmail.com>
2022-11-15 21:15:52 -0500

    add functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,17 @@
+int f(int n) {
+  int i = 0;
+  for (int j = 0; j < n; ++j) {
+    ++i;
+  }
+  return i;
+}
+
+int f_async(int n) {
+  // TODO
+  return 0;
+}
+
+int f_threads(int n) {
+  // TODO
+  return 0;
+}
