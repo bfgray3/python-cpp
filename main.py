86d32c9b6937d6d3525370f871e0407324cb4ba4
Bernie Gray <bfgray3@gmail.com>
2022-11-15 21:07:38 -0500

    main.py takes command line argumnt

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
+import argparse
 import ctypes
 import time
 from collections.abc import Sequence
 
 from python import functions
 
-N = 2**20 - 1
-
 
 def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", nargs="?", default=1 << 20 - 1, type=int)
+    args = parser.parse_args()
+
     python_start = time.perf_counter()
-    p = functions.f(N)
+    p = functions.f(args.n)
     python_end = time.perf_counter()
     print("python:", python_end - python_start)
 
     c_start = time.perf_counter()
     # TODO: try https://docs.python.org/3/library/ctypes.html#ctypes.PyDLL
-    c = ctypes.CDLL("./sharedlibrary.so").f(N)  # no GIL
+    c = ctypes.CDLL("./sharedlibrary.so").f(args.n)  # no GIL
     c_end = time.perf_counter()
     print("c:", c_end - c_start)
 
