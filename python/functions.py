fa5ae1acd0123b7f5714416efb669ebabcefd393
Bernie Gray <bfgray3@gmail.com>
2022-11-15 21:48:23 -0500

    add python threading implementation

--- python/functions.py
+++ python/functions.py
@@ -1,5 +1,18 @@
+import threading
+
+
 def f(n: int) -> int:
     i = 0
     for _ in range(n):
         i += 1
     return i
+
+
+def f_threads(n: int) -> int:
+    t1 = threading.Thread(target=f, args=(n,))
+    t2 = threading.Thread(target=f, args=(n,))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    return 0
