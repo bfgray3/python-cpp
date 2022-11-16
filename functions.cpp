d460773b0e34cb5223273acea4bad128c68f63a6
Bernie Gray <bfgray3@gmail.com>
2022-11-15 21:47:19 -0500

    threading function for cpp and extern "C"

--- functions.cpp
+++ functions.cpp
@@ -1,3 +1,9 @@
+#include <thread>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int f(int n) {
   int i = 0;
   for (int j = 0; j < n; ++j) {
@@ -8,10 +14,17 @@ int f(int n) {
 
 int f_async(int n) {
   // TODO
-  return 0;
+  return n;
 }
 
 int f_threads(int n) {
-  // TODO
+  // TODO: try jthread
+  std::thread t1{f, n}, t2{f, n};
+  t1.join();
+  t2.join();
   return 0;
 }
+
+#ifdef __cplusplus
+}
+#endif
